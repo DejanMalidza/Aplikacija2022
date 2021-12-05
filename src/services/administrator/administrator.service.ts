00358f3fd584ed5aed15b050256a9a81997e95ec
Dejan <dejanmalidza@gmail.com>
2021-12-05 14:43:07 +0100

    Create the adimnistrator table service

---
+++ src/services/administrator/administrator.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Administrator } from 'entities/administrator.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class AdministratorService {
+    constructor(
+        @InjectRepository(Administrator) 
+        private readonly adiministrator: Repository<Administrator>,
+    ) { }
+    
+    getAll(): Promise<Administrator[]> {
+        return this.adiministrator.find();
+    }
+
+    getById(id: number): Promise<Administrator> {
+        return this.adiministrator.findOne(id);
+    }
+}
+
+
